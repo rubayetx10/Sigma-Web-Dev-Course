bd947dcc64b736b273a3c05bcd6926985bd78859
rubayetx10 <rubayeth151@gmail.com>
2026-01-07 12:46:21 +0600

    Implement createCard function with view formatting
    Updated createCard function to include view formatting and dynamic HTML generation.

--- Video 73/script.js
+++ Video 73/script.js
@@ -1,6 +1,42 @@
-function createCard(title, cName, views, monthsOld, duration, thumbnail){
-    // Finish this function
+function createCard(title, cName, views, date, duration, thumbnail) {
+
+  // views format
+  let viewStr;
+  if (views >= 1000000) {
+    viewStr = (views / 1000000).toFixed(1) + "M views";
+  } 
+  else if (views >= 1000) {
+    viewStr = (views / 1000).toFixed(1) + "K views";
+  } 
+  else {
+    viewStr = views + " views";
+  }
+
+  let card = document.createElement("div");
+  card.className = "container";
+
+  card.innerHTML = `
+    <div class="thumbnail">
+      <img src="${thumbnail}">
+      <span class="duration">${duration}</span>
+    </div>
+
+    <div class="details">
+      <h3>${title}</h3>
+      <p>${cName}</p>
+      <p>${viewStr} • ${date} days ago</p>
+    </div>
+  `;
+
+  document.getElementById("cards").append(card);
 }
 
-
-createCard("Introduction to Backend | Sigma Web Dev video #2", "CodeWithHarry", 560000, 7, "31:22", "https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLACwWOixJVrKLFindK92kYMgTcQbw")
+// function call
+createCard(
+  "Introduction to Backend || Sigma Web Dev Video #73",
+  "CodeWithHarry",
+  560,
+  7,
+  "31:33",
+  "https://i.ytimg.com/vi/KtL-SQ20Q0s/hqdefault.jpg"
+);
